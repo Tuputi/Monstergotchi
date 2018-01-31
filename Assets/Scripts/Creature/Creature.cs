3fa9da06b916ad7759735ac440486c6f56f58798
Tove <tove.brantberg@gmail.com>
2018-01-31 19:17:45 +0200

    new touch plugin added

--- Assets/Scripts/Creature/Creature.cs
+++ Assets/Scripts/Creature/Creature.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using CreatureStates;
 using DigitalRuby.Tween;
+using TouchScript.Behaviors;
+using TouchScript.Gestures.TransformGestures;
+using System;
 
 public class Creature : MonoBehaviour {
 
@@ -149,6 +152,48 @@ public class Creature : MonoBehaviour {
 
 
 	//MOVEMENT/LOCATION LOGIC
+
+	private TransformGesture gesture;
+	private Transformer transformer;
+	private Rigidbody rb;
+
+	private void OnEnable()
+	{
+		// The gesture
+		gesture = GetComponent<TransformGesture>();
+		// Transformer component actually MOVES the object
+		transformer = GetComponent<Transformer>();
+		rb = GetComponent<Rigidbody>();
+
+		transformer.enabled = false;
+		rb.isKinematic = false;
+
+		// Subscribe to gesture events
+		gesture.TransformStarted += transformStartedHandler;
+		gesture.TransformCompleted += transformCompletedHandler;
+	}
+
+	private void OnDisable()
+	{
+		// Unsubscribe from gesture events
+		gesture.TransformStarted -= transformStartedHandler;
+		gesture.TransformCompleted -= transformCompletedHandler;
+	}
+
+	private void transformStartedHandler(object sender, EventArgs e)
+	{
+		// When movement starts we need to tell physics that now WE are moving this object manually
+		rb.isKinematic = true;
+		transformer.enabled = true;
+	}
+
+	private void transformCompletedHandler(object sender, EventArgs e)
+	{
+		transformer.enabled = false;
+		rb.isKinematic = false;
+		rb.WakeUp();
+	}
+
 
 	public void MoveToActionPoint(string myTargetType){
 		ActionPoint targetPoint = RoomManager.Instance.GetActionPoint (myTargetType);
